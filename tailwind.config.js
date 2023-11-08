f88150d74d6bc2024557c444abbdf2e74e75b587
Taniya Saini <taniyasaini6871@gmail.com>
2023-11-08 16:29:50 +0530

    Four DOM question completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   darkMode: 'class',
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        "bottomslider": "bottomslider 5s linear forwards"
+      },
+      keyframes:{
+        bottomslider:{
+          "100%":{
+            width:0
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
